b4d7b631d850a42a7172c84ae759b6f44d01e7f3
Ram Balachandran <a005.ram@gmail.com>
2026-07-10 07:06:06 -0500

    Updated Gnews to TOI for India

--- server.js
+++ server.js
@@ -149,7 +149,7 @@ app.get('/api/news/search', async (req, res) => {
 const GNEWS_URLS = {
   us: 'https://news.google.com/rss?hl=en-US&gl=US&ceid=US:en',
   world: 'https://news.google.com/rss/headlines/section/topic/WORLD?hl=en-US&gl=US&ceid=US:en',
-  in: 'https://news.google.com/rss?hl=en-IN&gl=IN&ceid=IN:en',
+  in: 'https://timesofindia.indiatimes.com/rssfeedstopstories.cms',
 };
 
 app.get('/api/news/google', async (req, res) => {
